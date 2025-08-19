6d1114c1dd9c31147034b9942a815b4887f08cfc
SaiyamJana <saiyamjana713@gmail.com>
2025-08-19 20:35:41 +0530

    3

--- frontPage/front.js
+++ frontPage/front.js
@@ -1,5 +1,5 @@
 function goTo(page) {
     // In the future, you can link this to actual pages
     alert(`This would take you to: ${page}`);
-    // window.location.href = page; // Uncomment when you have the pages ready
+    window.location.href = page; // Uncomment when you have the pages ready
 }
